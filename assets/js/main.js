02865d4096f78edba5dcee1a254a9232f43a3075
reglass2 <novikov.grigoriy@gmail.com>
2016-10-25 14:18:59 +0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,5 +185,18 @@
 			});
 			  
 		});
+	
+	// Accordion
+		$(document).ready(function(){
+			var acc = document.getElementsByClassName("accordion");
+			var i;
+
+			for (i = 0; i < acc.length; i++) {
+				acc[i].onclick = function(){
+					this.classList.toggle("active");
+					this.nextElementSibling.classList.toggle("show");
+				}
+			}			  
+		});
 
 })(jQuery);
